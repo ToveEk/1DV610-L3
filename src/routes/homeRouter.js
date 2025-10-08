615d97698ae9ce86774d3285f8b22ec162493464
Tove Ek <te222uu@student.lnu.se>
2025-10-08 15:37:23 +0200

    Add homeRouter and integrate with main router for handling home routes

---
+++ src/routes/homeRouter.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { HomeController } from '../controllers/homeController.js'
+
+const homeController = new HomeController()
+
+export const homeRouter = express.Router()
+
+homeRouter.get('/', (req, res) => {
+  homeController.index(req, res)
+})
---
+++ src/routes/router.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import { homeRouter } from './homeRouter.js'
+
+export const router = express.Router()
+
+router.use('/', homeRouter)

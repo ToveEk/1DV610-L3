dea1242402d409833c4292e4e38dd965eaaff200
Tove Ek <te222uu@student.lnu.se>
2025-10-14 14:40:00 +0200

    Add jsdoc comments to home controller

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,13 +1,22 @@
 /**
  * HomeController handles requests to the home page.
- * 
+ *
  * @module controllers/homeController
  * @author Tove Ek
  * @version 1.0.0
  */
 
+/**
+ * The HomeController class.
+ */
 export class HomeController {
-    renderHomePage(req, res) {
-        res.render('home/home')
-    }
+  /**
+   * Renders the home page.
+   *
+   * @param {object} req - The request object
+   * @param {object} res - The response object
+   */
+  renderHomePage (req, res) {
+    res.render('home/home')
+  }
 }

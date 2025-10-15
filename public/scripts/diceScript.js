ed40cc89c6f5c0df551a897c6da808232977dd52
Tove Ek <te222uu@student.lnu.se>
2025-10-15 12:19:21 +0200

    Add dice rolling script and update species selection page with comments

---
+++ public/scripts/diceScript.js
@@ -0,0 +1,16 @@
+/**
+ * Script for handling dice rolls on the species, class and ability score pages.
+ * Uses the Dice module to perform rolls and updates the UI accordingly.
+ *
+ * @module diceScript
+ * @author Tove Ek
+ * @version 1.0.0
+ */
+
+import * as DiceModule from 'DiceModule'
+
+const diceRoller = new DiceModule.Dice()
+
+// diceRoller.startRolling('d8')
+
+// TODO: Add event listeners to buttons and update the UI with roll results
